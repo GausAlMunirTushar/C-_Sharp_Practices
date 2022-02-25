b77f3a5fbd48baea8b16bb2c52433ebc6999f76e
Gaus Al Munir Tushar <contact@gausalmunirtushar.com>
2022-02-25 09:49:51 +0600

    c sharp hello world program run

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace HelloWorld
     static void Main(string[] args)
     {
       Console.WriteLine("Hello World!");    
+    /* The code below will print the words Hello World
+    to the screen, and it is amazing */
+      Console.WriteLine("Hello World!"); 
     }
   }
-}
+/*  */}
